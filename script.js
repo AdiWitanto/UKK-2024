d356f311801e32fe34d7ace6660f003659e8a00f
AdiWitanto <gurudaman32@gmail.com>
2024-11-19 20:00:54 +0700

    script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.querySelectorAll ('a[href^="#"]') .forEach (anchor => {
 anchor.addEventListener ('click' , function(e) {
 e.preventDefault ();
-document.querySelector (this.getAttribut ('href')) .scrollIntoview ({
+document.querySelector (this.getAttribute ('href')) .scrollIntoView ({
 behavior:'smooth'
 
 })
